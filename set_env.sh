36e88f409d444f6a69d0ba64e1def708df61c912
Pham Hung Cuong <cuongph10@fpt.com>
2024-12-01 20:25:57 +0700

    a

--- set_env.sh
+++ set_env.sh
@@ -1,7 +1,7 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
-export POSTGRES_USERNAME=postgres
-export POSTGRES_PASSWORD=postgres
+export POSTGRES_USERNAME=no-public
+export POSTGRES_PASSWORD=no-public
 export POSTGRES_HOST=database-main-project.cnrbfvmd0bbp.us-east-1.rds.amazonaws.com
 export POSTGRES_DB=postgres
 export AWS_BUCKET=micro-services-project
